47fc6f8f2fe6e8084c9bb3d2ebd5936d8224a545
BraulioPy <balcantararomo@gmail.com>
2025-02-03 16:41:06 -0600

    Creación de enumeraciónes

---
+++ Logistica/Enumeraciones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistica
+{
+    internal class Enumeraciones
+    {
+        public enum EstadoVehiculo
+        {
+            Disponible,
+            EnRuta,
+            EnMantenimiento,
+        }
+        public enum TipoLicencia
+        {
+            A,
+            B,
+            C,
+            D
+        }
+        public enum EstadoEntrega
+        {
+            Pendiente,
+            EnTransito,
+            Entregado,
+            Cancelado
+        }
+    }
+}
